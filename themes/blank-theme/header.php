93b70681661fc2289db87172279e49bd19731275
Tyle Gerard <tyler@tylergerard.com>
2015-09-06 13:39:53 -0600

    Added bootstrap 4.0 alpha.

--- themes/blank-theme/header.php
+++ themes/blank-theme/header.php
@@ -1,9 +1,10 @@
-<!doctype html>
+<!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
-  <title><?php wp_title( '-', true, 'right' ); echo esc_html( get_bloginfo('name') ); ?></title>
   <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width,initial-scale=1,user-scalable=no">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width,initial-scale=1">
+  <title><?php wp_title( '-', true, 'right' ); echo esc_html( get_bloginfo('name') ); ?></title>
   <link rel="pingback" href="<?php bloginfo('pingback_url') ?>" />
   <?php wp_head(); ?>
 </head>
@@ -13,4 +14,4 @@
     <div id="header">
       <h1>Header</h1>
     </div> <!-- end #header -->
-    <div id="content-wrapper">
+    <div id="content-wrapper" class="container-fluid">
